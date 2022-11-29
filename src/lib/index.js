0c63425f24ed4a59de0b94711462e5890703ba32
Lik Eduard <feft99@gmail.com>
2022-11-29 15:21:29 +0600

    Update

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,7 +1,6 @@
 import FlowView from './templates/FlowView'
 import FlowPage from './templates/FlowPage'
-import FlowPageVue from './templates/FlowPage.vue'
 
 export * from './registers'
 export * from './interaction'
-export { FlowView, FlowPage, FlowPageVue }
+export { FlowView, FlowPage }
